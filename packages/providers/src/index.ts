ff3e30c26b4032cbebd4f24f9cea0483566576b2
jrkosinski <john.kosinski@gmail.com>
2026-02-16 13:44:40 +0700

    merge conflict fixes

---
+++ packages/providers/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * AI provider integrations and authentication.
+ * Simple initial implementation for milestone: agent listens on one channel, sends updates on another.
+ */
+
+export * from './types.js';
+export * from './utils/provider-utils.js';
